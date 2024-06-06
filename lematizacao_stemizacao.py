e65cd9f81c6e697f4b50d80cae4d86cc32f6c21e
AndreSVasconcelos <andres.vasconcelos@gmail.com>
2024-06-06 14:30:30 -0300

    Lematização e Stemização

--- lematizacao_stemizacao.py
+++ lematizacao_stemizacao.py
@@ -4,13 +4,16 @@ import urllib.request
 import nltk
 import spacy
 
-# Carregando Spacy em Português
+# Carregando Spacy em Português -> Lematização
 pln = spacy.load('pt_core_news_sm')
 
 # Criar documento de texto a ser analisado
 documento = pln('Batatinha quando nasce se esparrama pelo chão')
 
+# Stemização trabalha com radicais
+stemmer = nltk.stem.RSLPStemmer()
+
 # Verifica como o spacy classificou as palavras
 for token in documento:
-    print(token.text, token.pos)
+    print(token.text, token.lemma_, token.pos, stemmer.stem(token.text))
 
