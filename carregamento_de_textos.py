61800d693eefd0eb0d570d0ae06978eaa023231a
AndreSVasconcelos <andres.vasconcelos@gmail.com>
2024-06-06 18:31:31 -0300

    Busca de textos

--- carregamento_de_textos.py
+++ carregamento_de_textos.py
@@ -3,6 +3,8 @@ import bs4 as bs
 import urllib.request
 import nltk
 import spacy
+from spacy.matcher import PhraseMatcher
+from IPython.core.display import HTML
 
 # Carregamento e limpeza de um texto de uma url
 dados = urllib.request.urlopen('https://pt.wikipedia.org/wiki/Harmonia_(m%C3%BAsica)')
@@ -15,4 +17,25 @@ for p in paragrafos:
     conteudo += p.text
 conteudo = conteudo.lower()
 
+# Busca de palavras com spacy
+pln = spacy.load('pt_core_news_sm')
+string='acorde'
+token_pesquisa = pln(string)
+matcher = PhraseMatcher(pln.vocab)
+matcher.add('SEARCH', None, token_pesquisa)
+doc = pln(conteudo)
+matches = matcher(doc)
 
+# Apresentando resultado
+numero_palavras = 10
+#display(HTML(f'<h1>{string.upper()}</h1>'))
+#display(HTML(f"""<p><strong>Resultados encontrados:</strong> {len(matches)}</p>"""))
+
+texto = ''
+for i in matches:
+    inicio = i[1] - numero_palavras
+    if inicio < 0:
+        inicio = 0
+    texto += str(f"...{doc[inicio:i[2] + numero_palavras]}...").replace(string, string.upper())
+    texto += '\n\n'
+print(texto)
